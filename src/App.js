a5f5efd512f90aee0ae3dfe834c6e18ea09da760
theharriet <goand123@gmail.com>
2021-10-04 17:48:56 +0900

    setting & how it works

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     hi
     </div>
   );
 }
 
 export default App;
+
+//React가 빠른 이유
+//react는 소스코드에 처음부터 HTML을 넣지 않고, HTMl에서 HTML을 추가하거나 제거한다
+// 빈 html을 불러왔다가 react가 html을 밀어넣음
+// react는 virtual임
