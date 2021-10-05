7974ba8045bcceb512244646f01612a7ad5988e9
theharriet <goand123@gmail.com>
2021-10-05 11:16:40 +0900

    About React component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
+//import React from "react"; //니코한텐 있던데 여긴 없어도 되는건가
+import Potato from "./Potato"; //이건있어야하네
 
 function App() {
   return (
     <div className="App">
      hi
+     <Potato />
     </div>
   );
 }
@@ -13,3 +16,6 @@ export default App;
 //react는 소스코드에 처음부터 HTML을 넣지 않고, HTMl에서 HTML을 추가하거나 제거한다
 // 빈 html을 불러왔다가 react가 html을 밀어넣음
 // react는 virtual임
+
+//component : HTML을 반환하는 함수
+// function App을 가지고 html을 반환할거야 여기서.
--- src/index.js
+++ src/index.js
@@ -9,4 +9,35 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
+// react는 component를 사용해서 html처럼 작성하려는 경우에 필요
+//component : HTML을 반환하는 함수
 
+//javascript와 html 사이의 이러한 조합을 -> jsx
+
+//****
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import App from './App';
+// import Potato from './Potato';
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App /><Potato />
+//   </React.StrictMode>,
+//   document.getElementById('potato')
+// );
+
+// //이렇게 하면 안돼 
+// //react application이 하나의 component만을 rendering해야하기 때문이야 
+// // 지금 그 컴포넌ㅌ트가 App. 그 안에 다른 컴포넌트를 넣어줘야 쓸수있음
+
+//App.js안에 <Potato />를 추가
+//import Potato from "./Potato";
+// function App() {
+//   return (
+//     <div className="App">
+//      hi
+//      <Potato />
+//     </div>
+//   );
+// }
