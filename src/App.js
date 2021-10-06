31f5cf710d4b651d06f9b84025d1c776f27578a2
theharriet <goand123@gmail.com>
2021-10-06 15:31:11 +0900

    Axios

--- src/App.js
+++ src/App.js
@@ -1,34 +1,38 @@
-//All you need to know about State
+//3.3 plannig the movie component
+//3.4 fetching movies from API / fetch보다 좋은거 Axios(fetch위에 있는 작은 layer)
 import React from "react";
+import axios from "axios";
 
 class App extends React.Component{
   state = {
-    count: 0
+    isLoading: true,
+    movies: [] //이렇게 쓰고 setTimeout에 book: true를 쓰면 에러가 날까? 안남. 미래에 쓰고자 하는 state를 선언하는건 필수가 아님 
   };
-  add = () => {
-    this.setState(current => ({ count: current.count + 1 }));
-  };
-  minus = () => {
-    this.setState(current => ({ count: current.count -1 }));
+
+  getMovies = async() => {
+   const movies = await axios.get("https://yts-proxy.now.sh/list_movies.json");
   }
 
+  //처음에 우리가 render를 하면 호출되는 life cycle method
   componentDidMount(){
-    console.log("component rendered");
+    // setTimeout(()=> {
+    //   this.setState({isLoading: false, book: true });
+    // }, 6000);
+    // fetch()
+   
+   // const movies = axios.get("https://yts-proxy.now.sh/list_movies.json");
+   //axios가 항상 빠르지는 않아 그래서 javascript에게 componetnDidMount함수가 끝날대 까지 약간 시간이 걸릴수 있다고 말해줘야함
+   //그래서 이걸 getMovies라는 함수에 넣어 async와 await도 넣어줌 await까지 기다려줌 그리고 호출
+    this.getMovies();
   }
-  componentDidUpdate(){
-    console.log("I just updated");
-  }
+//이제 우리가 할 일 : componentDidMount에서 data를 fetch
+//API로부터 data fetching 후에 we are ready 대신 map 만들어서 movie를 render.
 
-  componentWillUnmount(){
-    console.log("good bye");
-  }
   render(){
-    console.log("I'm redering");
-    return (
+    const { isLoading } = this.state;
+     return (
       <div>
-        <h1>The number is : {this.state.count}</h1>
-        <button onClick={this.add}>Add</button>
-        <button onClick={this.minus}>minus</button>
+       {isLoading ? "Loaidng..." : "We are ready"}
       </div>
     );
   }
@@ -36,19 +40,4 @@ class App extends React.Component{
 
 export default App;
 
-//react class component는 render만 있는게 아님
-// life cycle method도 있음. 기본적으로 react가 component를 생성하고 없애는 방법
-
-//mounting : component가 생성되어 DOM에 insert될 때 호출되는 메서드
-// -constructor():(reder전에)component가 mount될때, component가 screen에 표시될때, component가 너의 website에 갈때 호출
-// -componentDidMount : render()다음에 호출
-//updating : props or state가 바뀔때 호출, setState할때마다 (re-rendered 될 때)
-// -componentDidUpdate : setState를 호출 -> component 호출 -> render 호출 -> 업데이트완료 -> componentDidUpdate 실행
-//unmounting : component가 제거 될때, 페이지를 바꿀때, state를 사용해서 component가 교체될때 호출
-// -  componentWillUnmount()
-
-
-
-// add = () => {
-//   this.setState(current => ({ count: current.count + 1 }));
-// }; 클래스라 this.setState해줘야함
+//life cycle method -> mounting, updating, unmounting
